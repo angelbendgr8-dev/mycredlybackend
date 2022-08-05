f3cee2e06c837ae09b0fce9afc6d89d606cf21b4
benjamincalmerc <63807149+angelbendgr8-dev@users.noreply.github.com>
2022-08-05 07:44:38 +0100

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -25,7 +25,7 @@ Route::middleware('auth:sanctum')->group(function () {
     // Route::post('users/update/profile', 'updateProfilePics']);
     Route::prefix('users')->group(function () {
 
-        Route::controller(AuthController::class)->group(function () {
+        Route::controller(SettingController::class)->group(function () {
             Route::post('update/picture', 'updateProfilePics');
             Route::post('change/password', 'changePassword');
             Route::post('create/pin', 'createPin');
@@ -33,7 +33,7 @@ Route::middleware('auth:sanctum')->group(function () {
             Route::post('update/security', 'toggle2fa');
             Route::post('update/info', 'updateProfileInfo');
         });
-        
+
         Route::controller(BankAndCardController::class)->group(function () {
             Route::post('users/add/bank', 'updateProfilePics');
         });
